81895d68adb3a1479b4eb72a26adda7840b527ad
ezc9-tech <ezc9@outlook.com>
2025-08-05 21:48:56 -0500

    "This adds the ability to grab the data for the current day"

--- weather.js
+++ weather.js
@@ -10,11 +10,33 @@ document.addEventListener('DOMContentLoaded', function(){
     const api_key = ""
 
     searchButton.addEventListener('click', async function(){
-        const data = await getWeatherData();
-        if(data) displayFiveDayForecast(data);
+        const dataFive = await getFiveDayWeatherData();
+        const dataToday = await getTodayWeatherData();
+        if(dataFive) displayFiveDayForecast(dataFive);
     })
 
-    async function getWeatherData(){
+    async function getFiveDayWeatherData(){
+        if (cityInput.value === ''){
+            alert("The text box is empty")
+            return
+        } else {
+            const city = cityInput.value.trim()
+            const query = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`
+            try {
+                const response = await fetch(query)
+                if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json()
+                return data
+            } catch (error) {
+                console.error("Could not fetch weather data:", error);
+                alert("Failed to get weather data. Please check the city name and try again.");
+            }
+        }
+    }
+
+    async function getTodayWeatherData(){
         if (cityInput.value === ''){
             alert("The text box is empty")
             return
@@ -73,6 +95,10 @@ document.addEventListener('DOMContentLoaded', function(){
                     `
             fiveDayContainer.appendChild(dayElement)
         }
+    }
+
+    function displayTodaysWeather(data){
+
     }
 
 })
